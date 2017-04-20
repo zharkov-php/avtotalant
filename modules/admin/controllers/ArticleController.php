13301fe5a93b0a00b8cb1741d3fe8da73f8a31e2
Zharkov Andrey <zharkov.php@gmail.com>
2017-04-20 18:19:29 +0300

    Cread Uploud image in admin-panel for Article-model(One file pload at one article) || Создал загрузку картинок в админ-панеле для статей(одна картинка для одной статьиgit log)

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -2,12 +2,14 @@
 
 namespace app\modules\admin\controllers;
 
+use app\models\ImageUpload;
 use Yii;
 use app\models\Article;
 use app\models\ArticleSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
 
 /**
  * ArticleController implements the CRUD actions for Article model.
@@ -120,5 +122,24 @@ class ArticleController extends Controller
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
         }
+    }
+
+
+    public function actionSetImage($id)
+    {
+        $model = new ImageUpload;
+
+        if (Yii::$app->request->isPost)
+        {
+            $article = $this->findModel($id);
+            $file = UploadedFile::getInstance($model, 'image');
+
+            if($article->saveImage($model->uploadFile($file, $article->image)))
+            {
+                return $this->redirect(['view', 'id'=>$article->id]);
+            }
+        }
+
+        return $this->render('image', ['model'=>$model]);
     }
 }
